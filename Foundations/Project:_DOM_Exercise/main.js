725b7fbdcad9d084343b45be4b6cd69256186aea
olivier boucher <olivierboucher@MacBook-Pro-de-olivier.local>
2024-05-08 18:05:14 -0400

    Now i know the DOM

---
+++ Foundations/Project:_DOM_Exercise/main.js
@@ -0,0 +1,26 @@
+console.log("Illo");
+
+const container = document.querySelector("#container");
+const p = document.createElement("p");
+p.style.color = "red";
+p.textContent = "Hey I’m red!";
+container.appendChild(p);
+
+const h3 = document.createElement("h3");
+h3.style.color = "blue";
+h3.textContent = "I’m a blue h3!";
+container.appendChild(h3);
+
+const div = document.createElement("div");
+div.style.border = "1px solid black";
+div.style.backgroundColor = "pink";
+
+const div_h1 = document.createElement("h1");
+div_h1.textContent = "I’m in a div";
+div.appendChild(div_h1);
+
+const div_p = document.createElement("p");
+div_p.textContent = "ME TOO!";
+div.appendChild(div_p);
+
+container.appendChild(div);
